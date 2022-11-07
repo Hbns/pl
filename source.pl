eb2a389551a9655f59785052e789bb4fcb3c462b
hcns <vdohans@gmail.com>
2022-11-07 16:05:14 +0100

    progress

--- source.pl
+++ source.pl
@@ -7,13 +7,6 @@
 %:- use_module( [library(lists), library( 'io' ), library( 'fill' )] ).
 :- use_module([library(lists), io, fill]).
 
-% Board representation. 
-% There are eight ways to make a line of pieces > "Programming in Prolog p 93"
-
-% x is 1 2 or 3, list position 1,4,7.
-% y is 1,2 or 3, list position 2,3, 5,6, 8,9
-
-
 % is_cross/1 succeeds when its argument is the cross character in the representation.
 is_cross( Cross ) :- 
     Cross = x.
@@ -81,44 +74,91 @@ squ( X, Y, Piece ), where (X,Y) are the coordinates of the square given in the f
 two arguments, and Piece is one of the three square representation characters, indicating
 what if anything occupies the relevant square.*/
 
+% X is tested and can only be 1,2 or 3, Y is implicitly tested when calling row, since only row 1,2 or 3 exist.
 square(X, Y, Board, squ( X, Y, Piece)) :-
-    Y = 1, % test if y is 1, fail otherwise.
-    squ(X,Y,A) = member(row(X,A,B,C),Boad).
-    %row(N, A, B, C) = row(X, Board, row(N,A,B,C)),
-   % squ(X,Y,Piece) = squ(X,Y,A).
+    X = 1, 
+    row(Y, Board, row(Y,Piece,_,_)).
 
-    %member(squ(X,Y,Piece),[squ(X,Y, A)]).
+square(X, Y, Board, squ( X, Y, Piece)) :-
+    X = 2,
+    row(Y, Board, row(Y,_,Piece,_)).
 
+square(X, Y, Board, squ( X, Y, Piece)) :-
+    X = 3, 
+    row(Y, Board, row(Y,_,_,Piece)).
 
 /*empty_square/3 succeeds when its first two arguments are coordinates on the board, and the
 square they name is empty.*/
 
-% X is row number, Y is column number
 empty_square(X, Y, Board):-
-    member((X, Y, n), Board).
-
+    square(X, Y, Board, squ(X,Y,n)).
 
 /*initial_board/1 succeeds when its argument represents the initial state of the board.
 the initial board has nine empty squares*/
 
 initial_board(Board):-
-    Board = [(1,1,n),(1,2,n),(1,3,n),(2,1,n),(2,2,n),(2,3,n),(3,1,n),(3,2,n),(3,3,n)].
+    Board = [row(1,n,n,n),row(2,n,n,n),row(3,n,n,n),
+    col(1,n,n,n),col(2,n,n,n),col(3,n,n,n),
+    dia(top-to-bottom,n,n,n),dia(bottom-to-top,n,n,n)].
 
 /* empty_board/1 succeeds when its argument represents an uninstantiated board (ie with Pro-
 log variables in all the spaces). */
 
 empty_board(Board):-
-    Board = [(1,(1,_),(2,_),(3,_)),(2,1,_),(2,2,_),(2,3,_),(3,1,_),(3,2,_),(3,3,_)].
+    Board = [row(1,_,_,_),row(2,_,_,_),row(3,_,_,_),
+    col(1,_,_,_),col(2,_,_,_),col(3,_,_,_),
+    dia(top-to-bottom,_,_,_),dia(bottom-to-top,_,_,_)].
+
+/*and the winner is/2 succeeds when its first argument represents a board, and the second is
+a player who has won on that board. (Hint: use the predicates above here; you need 3
+clauses.)*/
+
+and_the_winner_is(Board, Player):-
+    row(_, Board, row(_, Player, Player, Player)).
+
+and_the_winner_is(Board, Player):-
+    column(_, Board, col(_, Player, Player, Player)).
+
+and_the_winner_is(Board, Player):-
+    diagonal(_, Board, dia(_, Player, Player, Player)).
+
 
 %%% TEST CODE %%%
 
+% testing for winner in a row, column or diagonal.
+test_and_the_winner_is(winner, all_tests):-
+    test_and_the_winner_isro(winner, row),
+    test_and_the_winner_isrx(winner, row),
+    test_and_the_winner_isco(winner, column),
+    test_and_the_winner_isdx(winner, diagonal).
+
+test_and_the_winner_isro(winner, row):-
+    and_the_winner_is([row(1,n,n,n),row(2,o,o,o),row(3,n,n,n),
+    col(1,n,n,n),col(2,n,n,n),col(3,n,n,n),
+    dia(top-to-bottom,n,n,n),dia(bottom-to-top,n,n,n)], o).
+
+test_and_the_winner_isrx(winner, row):-
+    \+ and_the_winner_is([row(1,n,n,n),row(2,o,o,o),row(3,n,n,n),
+    col(1,n,n,n),col(2,n,n,n),col(3,n,n,n),
+    dia(top-to-bottom,n,n,n),dia(bottom-to-top,n,n,n)], x).
+
+test_and_the_winner_isco(winner, column):-
+    and_the_winner_is([row(1,n,x,n),row(2,n,n,n),row(3,n,n,n),
+    col(1,n,n,n),col(2,o,o,o),col(3,n,n,n),
+    dia(top-to-bottom,n,n,n),dia(bottom-to-top,n,n,n)], o).
+
+test_and_the_winner_isdx(winner, diagonal):-
+    and_the_winner_is([row(1,n,x,n),row(2,n,n,n),row(3,n,n,n),
+    col(1,n,n,n),col(2,n,n,n),col(3,n,n,n),
+    dia(top-to-bottom,x,x,x),dia(bottom-to-top,n,n,n)], x).
+
 % true if row number exist.
 test_row(row, first_row):-
-    row(1,[row(1,x,n,o),row(2,x,x,o),row(3,n,o,n)],row(N,A,B,C)).
+    row(1,[row(1,x,n,o),row(2,x,x,o),row(3,n,o,n)],row(1,x,n,o)).
 
 % true if row number doesn't exist
 test_row(row, wrong_row):-
-    \+ row(5, [row(1,x,n,o),row(2,x,x,0),row(3,n,o,n)], row(N, A, B, C)).   
+    \+ row(5, [row(1,x,n,o),row(2,x,x,0),row(3,n,o,n)], row(5, x, o, n)).   
 
 % groups both tests above in one.    
 tests_row(row, all_tests):-
@@ -127,19 +167,19 @@ tests_row(row, all_tests):-
 
 % true if column number exist.
 test_column(column, first_column):-
-    column(1,[col(1,x,x,n),col(2,n,x,o),col(3,o,o,n)],col(N,A,B,C)).
+    column(1,[col(1,x,x,n),col(2,n,x,o),col(3,o,o,n)],col(1,x,x,n)).
 
 % true if col number doesn't exist
 test_column(column, wrong_column):-
-    \+ column(5, [col(1,x,x,n),col(2,n,x,o),col(3,o,o,n)], col(N, A, B, C)). 
+    \+ column(5, [col(1,x,x,n),col(2,n,x,o),col(3,o,o,n)], col(5, x,o,n)). 
 
 % true if diagonal "number" exist.
 test_diagonal(diagonal, top-to-bottom):-
-    diagonal(top-to-bottom,[dia(top-to-bottom,x,x,n),dia(bottom-to-top, n,x,o)], dia(N,A,B,C)).
+    diagonal(top-to-bottom,[dia(top-to-bottom,x,x,n),dia(bottom-to-top, n,x,o)], dia(top-to-bottom,x,x,n)).
 
 % true if diagonal "number" doesn'texist.
 test_diagonal(diagonal, top-to-top):-
-    diagonal(top-to-bottom,[dia(top-to-bottom,x,x,n),dia(bottom-to-top, n,x,o)], dia(N,A,B,C)).
+    \+ diagonal(top-to-bottom,[dia(top-to-bottom,x,x,n),dia(bottom-to-top, n,x,o)], dia(top-to-top,x,x,n)).
 
 
 
