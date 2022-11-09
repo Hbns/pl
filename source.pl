06cfa90897fd866a34237a67ceb1b79b4dd55ffe
hcns <vdohans@gmail.com>
2022-11-09 19:19:34 +0100

    progres

--- source.pl
+++ source.pl
@@ -4,23 +4,27 @@
 % - uninstatiated > output
 
 % Load library's
-%:- use_module( [library(lists), library( 'io' ), library( 'fill' )] ).
 :- use_module([library(lists), io, fill]).
 
+% is_cross(?Cross)
 % is_cross/1 succeeds when its argument is the cross character in the representation.
+
 is_cross( Cross ) :- 
     Cross = x.
 
+% is_nought(?Nought)
 % is_nought/1 succeeds when its argument is the nought character in the representation.
 
 is_nought( Nought ) :-
     Nought = o.
 
+%is_empty(?Empty)
 % is_empty/1 succeeds when its argument is the empty square character in the representation.
 
 is_empty( Empty ) :-
     Empty = n.
 
+% is_piece(?Piece)
 % is_piece/1 succeeds when its argument is either the cross character or the nought character.
 
 is_piece( Piece ) :-
@@ -28,56 +32,66 @@ is_piece( Piece ) :-
 is_piece( Piece ) :-
     is_cross( Piece ).
 
-% other_player/2 succeeds when both its arguments are player representation characters, but they are different.
+% other_player(?P1, ?P2)
+% other_player/2 succeeds when both its arguments are player representation characters,
+% but they are different.
 
 other_player( P1, P2) :-
     \+ (P1 = P2),
     is_piece( P1 ),
     is_piece( P2 ).
    
-
-/* row/3 succeeds when its first argument is a row number (between 1 and 3) and its second
+/*
+row(?Rn, ?Board, row(?N, ?A, ?B, ?C))
+row/3 succeeds when its first argument is a row number (between 1 and 3) and its second
 is a representation of a board state. The third argument will then be a term like this:
 row( N, A, B, C ), where N is the row number, and A, B, C are the values of the
-squares in that row. */
+squares in that row. 
+*/
 
 row(Rn, Board, row(N,A,B,C)) :-
-    % No check to see if Rn is beteen 1 and 3 since, if not then memebr will return false.
+    % No check to see if Rn is beteen 1 and 3 since, if not then member will return false.
     N=Rn,
     member(row(N,A,B,C),Board).
 
-/*column/3 succeeds when its first argument is a column number (between 1 and 3) and its
+/*
+column(?Cn, ?Board, col(?N, ?A, ?B, ?C))
+column/3 succeeds when its first argument is a column number (between 1 and 3) and its
 second is a representation of a board state. The third argument will then be a term like
 this: col( N, A, B, C ), where N is the column number, and A, B, C are the values
-of the squares in that column.*/
+of the squares in that column.
+*/
 
 column(Cn, Board, col(N,A,B,C)):-
-    Cn=1,
+    Cn = 1,
     N = Cn,
     row(1, Board, row(1,A,_,_)),
     row(2, Board, row(2,B,_,_)),
     row(3, Board, row(3,C,_,_)).
 
 column(Cn, Board, col(N,A,B,C)):-
-    Cn=2,
+    Cn = 2,
     N = Cn,
     row(1, Board, row(1,_,A,_)),
     row(2, Board, row(2,_,B,_)),
     row(3, Board, row(3,_,C,_)).
 
 column(Cn, Board, col(N,A,B,C)):-
-    Cn=3,
+    Cn = 3,
     N = Cn,
     row(1, Board, row(1,_,_,A)),
     row(2, Board, row(2,_,_,B)),
     row(3, Board, row(3,_,_,C)).
 
 
-/*diagonal/3 succeeds when its first argument is either top to bottom or bottom to top and
+/*
+diagonal(?D, ?Board, dia(?N, ?A, ?B, ?C)) 
+diagonal/3 succeeds when its first argument is either top-to-bottom or bottom-to-top and
 its second is a representation of a board state. The third argument will then be a
 term like this: dia( D, A, B, C ), where D is the direction of the line (as above),
 and A, B, C are the values of the squares in that diagonal. The diagonal direction (eg
-top-to-bottom) is moving from left to right.*/
+top-to-bottom) is moving from left to right.
+*/
 
 diagonal(D, Board, dia(N,A,B,C)) :-
     D = top-to-bottom,
@@ -93,13 +107,17 @@ diagonal(D, Board, dia(N,A,B,C)) :-
     row(2, Board, row(2,_,B,_)),
     row(3, Board, row(3,A,_,_)).
 
-/*square/4 succeeds when its first two arguments are numbers between 1 and 3, and its third
+/*
+square(?X, ?Y, ?Board, squ(?X, ?Y, ?Piece))
+square/4 succeeds when its first two arguments are numbers between 1 and 3, and its third
 is a representation of a board state. The fourth argument will then be a term like this:
 squ( X, Y, Piece ), where (X,Y) are the coordinates of the square given in the first
 two arguments, and Piece is one of the three square representation characters, indicating
-what if anything occupies the relevant square.*/
+what if anything occupies the relevant square.
+*/
 
-% X is tested and can only be 1,2 or 3, Y is implicitly tested when calling row, since only row 1,2 or 3 exist.
+% X is tested and can only be 1,2 or 3, 
+% Y is implicitly tested when calling row, If Y is not 1,2 or 3, row returns false.
 square(X, Y, Board, squ( X, Y, Piece)) :-
     X = 1, 
     row(Y, Board, row(Y,Piece,_,_)).
@@ -112,31 +130,38 @@ square(X, Y, Board, squ( X, Y, Piece)) :-
     X = 3, 
     row(Y, Board, row(Y,_,_,Piece)).
 
-/*empty_square/3 succeeds when its first two arguments are coordinates on the board, and the
-square they name is empty.*/
+/*
+empty_square(?X, ?Y, ?Board)
+empty_square/3 succeeds when its first two arguments are coordinates on the board, 
+and the square they name is empty.
+*/
 
 empty_square(X, Y, Board):-
     square(X, Y, Board, squ(X,Y,n)).
 
-/*initial_board/1 succeeds when its argument represents the initial state of the board.
-the initial board has nine empty squares*/
+/*
+initial_board(?Board)
+initial_board/1 succeeds when its argument represents the initial state of the board.
+the initial board has nine empty squares represented by n (nothing).
+*/
 
 initial_board(Board):-
-    Board = [row(1,n,n,n),row(2,n,n,n),row(3,n,n,n),
-    col(1,n,n,n),col(2,n,n,n),col(3,n,n,n),
-    dia(top-to-bottom,n,n,n),dia(bottom-to-top,n,n,n)].
+    Board = [row(1,n,n,n),row(2,n,n,n),row(3,n,n,n)].
 
-/* empty_board/1 succeeds when its argument represents an uninstantiated board (ie with Pro-
-log variables in all the spaces). */
+/* 
+empty_board(?Board)
+empty_board/1 succeeds when its argument represents an uninstantiated board 
+(ie with Prolog variables in all the spaces). 
+*/
 
 empty_board(Board):-
-    Board = [row(1,_,_,_),row(2,_,_,_),row(3,_,_,_),
-    col(1,_,_,_),col(2,_,_,_),col(3,_,_,_),
-    dia(top-to-bottom,_,_,_),dia(bottom-to-top,_,_,_)].
+    Board = [row(1,_,_,_),row(2,_,_,_),row(3,_,_,_)].
 
-/*and the winner is/2 succeeds when its first argument represents a board, and the second is
-a player who has won on that board. (Hint: use the predicates above here; you need 3
-clauses.)*/
+/*
+and_the_winner_is(?Board, ?Player)
+and the winner is/2 succeeds when its first argument represents a board, and the second is
+a player who has won on that board. 
+*/
 
 and_the_winner_is(Board, Player):-
     row(_, Board, row(_, Player, Player, Player)).
@@ -147,9 +172,12 @@ and_the_winner_is(Board, Player):-
 and_the_winner_is(Board, Player):-
     diagonal(_, Board, dia(_, Player, Player, Player)).
 
-/*no more free squares/1 succeeds if the board represented in its argument has no empty
-squares in it.*/
-
+/*
+no_more_free_squares(?Board)
+no more free squares/1 succeeds if the board represented in its argument
+has no empty squares in it.
+*/
+ 
 no_more_free_squares(Board):-
     \+ empty_square(1,1,Board),
     \+ empty_square(1,2,Board),
@@ -161,7 +189,7 @@ no_more_free_squares(Board):-
     \+ empty_square(3,2,Board),
     \+ empty_square(3,3,Board).
 
-% 3.6 Running a game for 2 human players.
+%% 3.6 Running a game for 2 human players %%
 
 playHH :-
     welcome,
@@ -169,6 +197,12 @@ playHH :-
     display_board( Board ),
     is_cross( Cross ),
     playHH( Cross, Board ).
+
+/*
+change_player(?Player, ?NewPlayer)
+change_player/2 succeeds when argument one and two are valid pieces but not equal.
+given Player, NewPlayer will be the other valid Piece.
+*/
 
 change_player(Player, NewPlayer):-
     is_cross(Player),
@@ -179,24 +213,24 @@ change_player(Player, NewPlayer):-
     is_cross(NewPlayer).
 
 /*
+playHH(?Player, ?Board)
 playHH/2 is recursive. It has two arguments: a player, the first, and a board state, the
 second. For this section of the practical, it has three possibilities:
-1. The board represents a winning state, and we have to report the winner. Then we
-are finished.
-2. There are no more free squares on the board, and we have to report a stalemate.
-Again, we are finished.
-3. We can get a (legal) move from the player named in argument 1, fill the square
-he or she gives, switch players, display the board and then play again, with the
-updated board and the new player. */
+*/
 
+% 1. The board represents a winning state, and we have to report the winner. 
 playHH(Player, Board):-
     and_the_winner_is(Board, Player),
     report_winner(Player). 
 
+% 2. There are no more free squares on the board, and we have to report a stalemate.
 playHH(_, Board):-
     no_more_free_squares(Board),
     report_stalemate.
 
+% 3. We can get a (legal) move from the player named in argument 1, fill the square
+% he or she gives, switch players, display the board and then play again, 
+% with the updated board and the new player.
 playHH(Player, Board):-
     get_legal_move( Player, X, Y, Board ),
     fill_square( X, Y, Player, Board, NewBoard ),
@@ -204,7 +238,7 @@ playHH(Player, Board):-
     display_board(NewBoard),
     playHH(NewPlayer, NewBoard).
 
-%% 4 Running a game for 1 human and the computer.
+%% 4 Running a game for 1 human and the computer %%
 
 playHC :-
     welcome,
@@ -213,15 +247,24 @@ playHC :-
     is_nought( Nought ),
     playHC( Nought, Board ).
 
+/*
+playHH(?Player, ?Board)
+playHH/2 is recursive. It has two arguments: a player, the first, and a board state, the
+second. For this section of the practical, it has three possibilities:
+*/
+
+% 1. The board represents a winning state, and we have to report the winner. 
 playHC(Player, Board):-
     and_the_winner_is(Board, Player),
     report_winner(Player). 
 
+% 2. There are no more free squares on the board, and we have to report a stalemate.
 playHC(_, Board):-
     no_more_free_squares(Board),
     report_stalemate.
 
-/*The current player is x, we can get a (legal) move, fill the square, display the board,
+/*
+3.1 The current player is x, we can get a (legal) move, fill the square, display the board,
 and play again, with the new board and with nought as player.
 */
 
@@ -234,7 +277,7 @@ playHC(Player, Board):-
     playHC(NewPlayer, NewBoard).
 
 /*
-The current player is o, we can choose a move (see below), we tell the user what
+3.2 The current player is o, we can choose a move (see below), we tell the user what
 move we’ve made (see io library), we can fill the square, display the board, and
 play again, with the new board and with cross as player.
 */
@@ -252,7 +295,7 @@ playHC(Player, Board):-
 
 % 1. If there is a winning line for self, then take it;
 
-% winning horizontal line?
+% winning horizontal line for player?
 choose_move(Player, X, Y, Board):-
     X = 1,
     member(row(Y,n,Player,Player),Board).
@@ -265,7 +308,7 @@ choose_move(Player, X, Y, Board):-
     X = 3,
     member(row(Y,Player,Player,n),Board).
 
-% winning vertical line? 
+% winning vertical line for player? 
 choose_move(Player, X, Y, Board):-
     Y = 1,
     column(X, Board, col(X,n,Player,Player)).
@@ -278,7 +321,7 @@ choose_move(Player, X, Y, Board):-
     Y = 3,
     column(X, Board, col(X,Player,Player,n)).
 
-% winning diagonal line?
+% winning diagonal line for player?
 choose_move(Player, X, Y, Board):-
     X = 1,
     Y = 1,
@@ -394,7 +437,7 @@ choose_move(Player, X, Y, Board):-
 
 % 3. If the middle space is free, then take it;
 choose_move(_, X,Y,Board):-
-    X = 2,
+    X = 2, % (2,2) is the middle space on the board.
     Y = 2,
     empty_square(X,Y, Board).
 
@@ -430,21 +473,21 @@ playSS :-
     initial_board( Board ),
     display_board( Board ),
     is_nought( Nought ),
-    playHC( Nought, Board ).
+    playSS( Nought, Board ).
 
+% 1. The board represents a winning state, and we have to report the winner. 
 playSS(Player, Board):-
     and_the_winner_is(Board, Player),
     report_winner(Player). 
 
-/*playSS/2 We replace the second step of playHH/2 or playHC/2 above as follows:
-2. If no possible win is available, report a stalemate. Then we have finished.*/
+% 2. If no possible win is available, report a stalemate. 
 playSS(Player, Board):-
-    \+ possible_win(Player, Board),
+    possible_win(Player, Board),
     report_stalemate.
 
-/*The current player is x, we can get a (legal) move, fill the square, display the board,
-and play again, with the new board and with nought as player.
-*/
+
+% 3. The current player is x, we can get a (legal) move, fill the square, display the board,
+% and play again, with the new board and with nought as player.
 
 playSS(Player, Board):-
     is_cross(Player),
@@ -454,15 +497,20 @@ playSS(Player, Board):-
     display_board(NewBoard),
     playSS(NewPlayer, NewBoard).
 
-/*possible win/2 is recursive. It succeeds if the addition of one square to the board (rep-
+/*
+possible_win(?Player, ?Board)
+possible win/2 is recursive. It succeeds if the addition of one square to the board (rep-
 resented in the second argument) yields a win for a player (represented in the first
 argument). Alternatively, it succeeds if the result of adding one square to the board
 leads to a possible win, swapping players as it goes. In total, it succeeds if any player
-can win from the current position, allowing for whose move it is now.*/
+can win from the current position, allowing for whose move it is now.
+*/
 
-possible_win(Player, square(X, Y, Board, squ( X, Y, Player))):-
+possible_win(Player, Board):-
+    choose_move(Player, X,Y, Board),
     fill_square( X, Y, Player, Board, NewBoard ),
-    and_the_winner_is(NewBoard, Player).
+    and_the_winner_is(NewBoard, Player),
+    possible_win(Player, NewBoard).
 
 
 %%% only TEST CODE underneath.. %%%
