9f96816eb48170191b4c7e6b77954003a65c44d2
hcns <vdohans@gmail.com>
2023-01-02 13:45:05 +0100

    bu

--- sorb.pl
+++ sorb.pl
@@ -1,5 +1,16 @@
 /* use these as input
 
+list_of_inputs:
+
+[[table,for,2,at,20,’:’,00,on,18,march],
+[we,would,like,a,table,for,5,preferably,at,8,pm,on,18,’/’,03],
+[please,can,we,have,a,table,for,3,for,the,theatre,menu,on,march,18,th],
+[can,i,book,a,table,at,9,pm,for,2,people,on,the,18,th,of,march,for,the,standard,menu,please],
+[reserve,us,a,table,on,march,18,for,a,party,of,4,for,the,standard,menu]
+[9,people,on,18,th,of,march],
+[book,6,of,us,in,on,18,march,at,20,’:’,00],
+[reservation,for,7,on,march,18,preferably,for,standard,menu,at,7,oclock]]
+
 reservation(A, B, C, D, [table,for,2,at,20,’:’,00,on,18,march],[]).
 reservation(A, B, C, D, [we,would,like,a,table,for,5,preferably,at,8,pm,on,18,’/’,03],[]).
 reservation(A, B, C, D, [please,can,we,have,a,table,for,3,for,the,theatre,menu,on,march,18,th],[]).
@@ -10,6 +21,10 @@ reservation(A, B, C, D, [book,6,of,us,in,on,18,march,at,20,’:’,00],[]).
 reservation(A, B, C, D, [reservation,for,7,on,march,18,preferably,for,standard,menu,at,7,oclock],[]).
 
 */
+
+% -> translate list of inputs with dcg
+
+
 reservation(A,B,C,D) --> reservation_part(A), reservation_part(B), reservation_part(C), reservation_part(D).
 
 reservation_part(A) --> no_information, information(A), no_information.
@@ -32,7 +47,7 @@ no_information --> [_], no_information.
 persons(P) --> [for] ,number_of_people(P).
 persons(P) --> number_of_people(P), [people].
 persons(P) --> number_of_people(P), [of, us].
-number_of_people(P) --> [P], {integer(P), P < 5}. % reservation not fitting one table are not allowed.
+number_of_people(P) --> [P], {integer(P), P < 5}. % reservations not fitting one table(P<5) are not allowed.
 number_of_people(P) --> [a, party, of], number_of_people(P).
 
 % preferably? 
@@ -46,76 +61,84 @@ req_date(D) --> [_Word], req_date(D).
 menu(M) --> [M, menu].
 
 
+
 %% Constraint System %%
 :- use_module( [library(clpfd),library(lists)] ).
+% to use block, we need:
+:- expects_dialect( sicstus ).
+
 % Opens @19h, closes @23h
 % 3 tables, one table can seat 2,3,or 4.
-% eat time = 1h for theatre menu, 2h for std menu
+% eat time = 1h for theatre menu, 2h for standard menu
 % the restaurant optimizes bookings, eg: prefer max persons
 
-book_tables(DiningList) :-
-    length(DiningList, 12), % 4 theather menus times 3 tables.
-    length(Dinings, 12),
-    all_different(Dinings),
-    constrain_dinings(Dinings, Variables, DiningList),
-    link_dinings(DiningList),
-    labeling([ffc],Variables).
+res_request(3,1,18).
 
-constrain_time(T):-
-T in 19..22.
+% dining(dnumber, menu)
 
-constrain_table_number(N):-
-    N in 1..3.
-
-% table(table_number, persons, time, date, menu)
-% :- block table(-,-,-,-,-).
-
-tables(3).
-
-table(1, Persons, Time, Date, Menu).
-table(2, Persons, Time, Date, Menu).
-table(3, Persons, Time, Date, Menu).
-
-% dining(persons, time, date, menu)
-
+:- block dining(-,-).
+% table1
 dining(1,1).
 dining(1,2).
+dining(2,1).
+dining(2,2).
+dining(3,1).
+dining(3,2).
+dining(4,1).
 
-% should recieve this from dcg.
-dining(Dnumber, Persons, Time, Date, Menu).
+% table2
+dining(5,1).
+dining(5,2).
+dining(6,1).
+dining(6,2).
+dining(7,1).
+dining(7,2).
+dining(8,1).
+
+% table3
+dining(9,1).
+dining(9,2).
+dining(10,1).
+dining(10,2).
+dining(11,1).
+dining(11,2).
+dining(12,1).
+
+%number of tables
+tables(3).
 
 constrain_dinings([],[],[]).
 constrain_dinings([Dining|Dinings],
-    [Time, Menu, Table | Variables],% variables to drive constrained search, wat we want to fin out.
-    [dining(Dnumber, Persons, Date, Time, Menu, Table)|DiningList]):-
-        dining(Dnumber, Persons, Time, Date, Menu),
+    [Time, Menu | Variables],% variables to drive constrained search, wat we want to find out.
+    [dining(Dnumber, Persons, Date, Time, Menu)|DiningList]):-
+        dining(Dnumber, Menu),
+        %res_request(Persons, Menu, Date),
         tables(MaxTables),
+        Persons in 1..4,
+        Date in 15..20,
         Time in 19..22,
         Menu in 1..2, %1h theatre menu eating time, standard menu 2h eating time.
-        Table in 1..3,
         constrain_dinings(Dinings, Variables, DiningList).
 
 link_dinings([]).
 link_dinings([_]).
-link_dinings( [dining(_, Persons1, Date1, Time1, Menu1, Table1),
-        dining(Dining2, Persons2, Date2, Time2, Menu2, Table2 )|Dinings]):-
-            Table2 #>= Table1,
-            Table2 #=< Table1 + 1,
-            (Table1 #= Table2) #==> (Time1 #\ Time2),
-            link_dinings([dining(Dining2, Persons2, Date2, Time2, Menu2, Table2 )|Dinings]).
+link_dinings( [dining(Dining1, Persons1, Date1, Time1, Menu1),
+        dining(Dining2, Persons2, Date2, Time2, Menu2)|Dinings]):-
+            Dining2 #> Dining1,
+            %Time2 #> Time1,
+            ( Menu1 #= 1 ) #<==> ( Time2 #= Time1 + 1 ),
+            %( Menu1 #= 2 ) #==> ( Time2 #= Time1 + 2 ),
+            link_dinings([dining(Dining2, Persons2, Date2, Time2, Menu2)|Dinings]).
 
-
+book_tables(DiningList) :-
+            length(DiningList, 12), % 4 theather menus times 3 tables, a maximum of 12 dinings per evening.
+            length(Dinings, 12),
+            all_different(Dinings), % do not double book a dining slot.
+            constrain_dinings(Dinings, Variables, DiningList),
+            link_dinings(DiningList),
+            labeling([ffc],Variables).
     
 
-
-constrain_tables([],[],[]).
-constrain_tables([Table|Tables], [Persons, Time, Date, Menu |Variables],[table(Number, Persons, Time, Date, Menu)|TableList]):-
-    Number in 1..3,
-    Person in 2..4,
-    Time in 19..22,
-    Date in 1..31,
-    Menu in 1..2,% eats 1hour or 2 hours.
-    constrain_tables(Tables, Variables, TableList).
 
 % control order => table 1, table2, table3 or minimise opening hour, close after last, or minimise simultanous guests?
 
